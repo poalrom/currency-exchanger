0ea3e7f4b083a2d5eaad5df4918e9272dd21e705
Alex Popkov <poalrom@yandex-team.ru>
2022-10-23 18:55:16 +0300

    feat(build): add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { VitePWA as vitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [splitVendorChunkPlugin(), react()],
+    plugins: [splitVendorChunkPlugin(), react(), vitePWA()],
     base: "./",
 });
